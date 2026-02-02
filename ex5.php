0a3f5df83f7badc844c73c72426ccc0a5014902a
annamu28 <anna.mugu28@gmail.com>
2026-02-02 08:37:11 +0200

    Add files via upload

---
+++ ex5.php
@@ -0,0 +1,26 @@
+<?php
+
+$sampleData = [
+    ['type' => 'apple', 'weight' => 0.21],
+    ['type' => 'orange', 'weight' => 0.18],
+    ['type' => 'pear', 'weight' => 0.16],
+    ['type' => 'apple', 'weight' => 0.22],
+    ['type' => 'orange', 'weight' => 0.15],
+    ['type' => 'pear', 'weight' => 0.19],
+    ['type' => 'apple', 'weight' => 0.09],
+    ['type' => 'orange', 'weight' => 0.24],
+    ['type' => 'pear', 'weight' => 0.13],
+    ['type' => 'apple', 'weight' => 0.25],
+    ['type' => 'orange', 'weight' => 0.08],
+    ['type' => 'pear', 'weight' => 0.20],
+];
+
+function getAverageWeightsByType(array $list): array {
+
+    // kood tuleb siia
+
+    return [];
+}
+
+// testimiseks
+// print_r(getAverageWeightsByType($sampleData));
