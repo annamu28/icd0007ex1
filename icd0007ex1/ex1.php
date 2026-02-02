42ae84afb807933c8f6cee84251a731f5a110977
annamu28 <anna.mugu28@gmail.com>
2026-02-02 08:35:55 +0200

    Delete icd0007ex1 directory

--- icd0007ex1/ex1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$numbers = [1, 2, '3', 6, 2, 3, 2, 3];
-
-$count = 0;
-
-// kood tuleb siia
-
-print "Found it $count times";
--- icd0007ex1/ex7.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function getDaysUnderTemp(int $targetYear, float $targetTemp): float {
-
-    // kood tuleb siia
-
-    return 0;
-}
